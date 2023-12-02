a1ef13e44316d6db3b9d6687b9992f18529b28bf
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-12-03 00:50:22 +0100

    day 1 part 2 done.

--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,55 +1,54 @@
+use std::ops::Add;
+use tracing_subscriber::fmt::format;
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(
-    input: &str,
-) -> miette::Result<String, AocError> {
+pub fn process(input: &str) -> miette::Result<String, AocError> {
     let output =
         input.lines().map(process_line).sum::<u32>();
-
     Ok(output.to_string())
 }
 
 fn process_line(line: &str) -> u32 {
-    let mut index = 0;
-    let line_iter = std::iter::from_fn(move || Some({
-        let reduced_line = &line[index..];
-
-        let result = if reduced_line.starts_with("one") {
+    let mut resulting: Vec<char> = vec![];
+    for (index, c) in line.chars().enumerate() {
+        let red = &line[index..];
+        let result = if red.starts_with("one") || red.starts_with("1") {
             '1'
-        } else if reduced_line.starts_with("two") {
+        } else if red.starts_with("two") {
             '2'
-        } else if reduced_line.starts_with("three") {
+        } else if red.starts_with("three") {
             '3'
-        } else if reduced_line.starts_with("four") {
+        } else if red.starts_with("four")  {
             '4'
-        } else if reduced_line.starts_with("five") {
+        } else if red.starts_with("five") {
             '5'
-        } else if reduced_line.starts_with("six") {
+        } else if red.starts_with("six") {
             '6'
-        } else if reduced_line.starts_with("seven") {
+        } else if red.starts_with("seven") {
             '7'
-        } else if reduced_line.starts_with("eight") {
+        } else if red.starts_with("eight") {
             '8'
-        } else if reduced_line.starts_with("nine") {
+        } else if red.starts_with("nine") {
             '9'
+        } else if red.chars().next().unwrap().is_numeric() {
+            red.chars().next().unwrap()
         } else {
-             //let result = reduced_line.chars().next();
-            //result.unwrap()
             '0'
         };
-        index += 1;
-        result
-    }));
-    let mut it = line_iter.filter_map(|char| char.to_digit(10));
-    let first = it.next().expect("should be a number");
 
-    match it.last() {
-        Some(num) => format!("{first}{num}"),
-        None => format!("{first}{first}"),
+        if result != '0' {
+            resulting.push(result);
+        }
     }
-        .parse::<u32>()
-        .expect("should be a valid number")
+    let first = resulting[0];
+    let last = resulting[resulting.len() - 1];
+
+    format!("{}{}", first, last).parse().unwrap()
+}
+
+fn begins_with(line: &str, str: &str) -> bool {
+    line.starts_with(str)
 }
 
 #[cfg(test)]
