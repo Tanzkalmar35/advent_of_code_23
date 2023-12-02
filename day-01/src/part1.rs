eabb55f64dc08dadab62dea4016411ad57ddffda
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-12-02 19:21:13 +0100

    day 2 part 1 done.

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,22 +1,23 @@
-use std::ptr::null;
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
     let result = input
-        .lines()
+        .lines()// Gives us each line individually
         .map(|line| {
-            let mut it = line.chars().filter_map(|char| {
-                char.to_digit(10)
+            // line.chars() effectively splits the line into single chars
+            let mut iterator = line.chars().filter_map(|char| {
+                // Try to cast the chars to a number of the decimal system
+                char.to_digit(10) // Only numbers will be casted
             });
-            let first_num = it.next().expect("This should be a number.");
-            match it.last() {
+            let first_num = iterator.next().expect("This should be a number.");
+            match iterator.last() {
                 Some(num) => format!("{first_num}{num}"),
-                None => format!("{first_num}{first_num}"),
+                None => format!("{first_num}{first_num}"), // If we only have one number
             }
                 .parse::<u32>()
                 .expect("This should be a valid number.")
-        }).sum::<u32>();
+        }).sum::<u32>(); // summing all lines
     Ok(result.to_string())
 }
 
--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -2,9 +2,54 @@ use crate::custom_error::AocError;
 
 #[tracing::instrument]
 pub fn process(
-    _input: &str,
+    input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    let output =
+        input.lines().map(process_line).sum::<u32>();
+
+    Ok(output.to_string())
+}
+
+fn process_line(line: &str) -> u32 {
+    let mut index = 0;
+    let line_iter = std::iter::from_fn(move || Some({
+        let reduced_line = &line[index..];
+
+        let result = if reduced_line.starts_with("one") {
+            '1'
+        } else if reduced_line.starts_with("two") {
+            '2'
+        } else if reduced_line.starts_with("three") {
+            '3'
+        } else if reduced_line.starts_with("four") {
+            '4'
+        } else if reduced_line.starts_with("five") {
+            '5'
+        } else if reduced_line.starts_with("six") {
+            '6'
+        } else if reduced_line.starts_with("seven") {
+            '7'
+        } else if reduced_line.starts_with("eight") {
+            '8'
+        } else if reduced_line.starts_with("nine") {
+            '9'
+        } else {
+             //let result = reduced_line.chars().next();
+            //result.unwrap()
+            '0'
+        };
+        index += 1;
+        result
+    }));
+    let mut it = line_iter.filter_map(|char| char.to_digit(10));
+    let first = it.next().expect("should be a number");
+
+    match it.last() {
+        Some(num) => format!("{first}{num}"),
+        None => format!("{first}{first}"),
+    }
+        .parse::<u32>()
+        .expect("should be a valid number")
 }
 
 #[cfg(test)]
@@ -13,9 +58,15 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "\
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen";
+        assert_eq!("281", process(input)?);
         Ok(())
     }
 }
