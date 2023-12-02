dc6dabcb33296258f327e841c9e06ce55768f85e
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-12-02 19:21:43 +0100

    day 2 part 1 done.

---
+++ day-02/src/part1.rs
@@ -0,0 +1,89 @@
+use std::collections::BTreeMap;
+use nom::bytes::complete::tag;
+use nom::character::complete;
+use nom::character::complete::{alpha1, digit1, line_ending};
+use nom::multi::separated_list1;
+use nom::IResult;
+use nom::sequence::{preceded, separated_pair};
+use crate::custom_error::AocError;
+
+#[derive(Debug)]
+struct Cube<'a> {
+    color: &'a str,
+    amount: u32,
+}
+
+#[derive(Debug)]
+struct Game<'a> {
+    id: &'a str,
+    rounds: Vec<Vec<Cube<'a>>>,
+}
+
+impl<'a> Game<'a> {
+    fn is_valid(&self, map: &BTreeMap<&str, u32>) -> Option<u32> {
+        self.rounds.iter().all(|round| {
+            round.iter().all(|shown_cube| {
+                shown_cube.amount <= *map.get(shown_cube.color).expect("a valid cube")
+            })
+        })
+            .then_some(
+                self.id.parse::<u32>().expect(
+                    "game id should be a parsable u32",
+                ),
+            )
+    }
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let map = BTreeMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14)
+    ]);
+    let games = parse_games(input).expect("should parse");
+    Ok(games.1.iter().filter_map(|game| game.is_valid(&map)).sum::<u32>().to_string())
+}
+
+// structure: game: round(cube); round(cube)...
+fn parse_games(input: &str) -> IResult<&str, Vec<Game>> {
+    let (input, games) = separated_list1(line_ending, game)(input)?;
+    Ok((input, games))
+}
+
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+fn game(input: &str) -> IResult<&str, Game> {
+    let (input, id) =
+        preceded(tag("Game "), digit1)(input)?;
+    let (input, rounds) =
+        preceded(tag(": "), separated_list1(tag("; "), round))(input)?;
+    Ok((input, Game { id, rounds }))
+}
+
+// 3 blue, 4 red
+fn round(input: &str) -> IResult<&str, Vec<Cube>> {
+    let (input, cubes) = separated_list1(tag(", "), cube)(input)?;
+    Ok((input, cubes))
+}
+
+// 4 red
+fn cube(input: &str) -> IResult<&str, Cube> {
+    let (input, (amount, color)) = separated_pair(complete::u32, tag(" "), alpha1)(input)?;
+    Ok((input, Cube { color, amount }))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        assert_eq!("8", process(input)?);
+        Ok(())
+    }
+}
