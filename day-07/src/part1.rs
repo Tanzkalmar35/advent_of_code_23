fc72acba137fbd8e0441fb2f6310cfa4f951bce1
Tanzkalmar35 <fabianholler2005@gmail.com>
2023-12-08 17:26:03 +0100

    working on day-07 part1

---
+++ day-07/src/part1.rs
@@ -0,0 +1,77 @@
+use crate::custom_error::AocError;
+
+enum Cards {
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    T = 10,
+    J = 11,
+    Q = 12,
+    K = 13,
+    A = 14,
+}
+
+enum Types {
+    FiveOfAKind = 7,
+    FourOfAKind = 6,
+    FullHouse = 5,
+    ThreeOfAKind = 4,
+    TwoPair = 3,
+    OnePair = 2,
+    HighCard = 1,
+    None = 0
+}
+
+struct Hand<'a> {
+    hand: &'a str,
+    hand_type: Types,
+}
+
+impl Hand {
+    pub fn from(hand: &str) -> Self {
+        Self { hand, hand_type: Types::None }
+    }
+
+    pub fn get_type(&self) -> Self {
+        let map = self.hand.split("").map(|char| {
+
+        });
+        todo!()
+    }
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let res = input.lines().map(|line| process_line).sum::<u32>();
+    Ok(res.to_string())
+}
+
+pub fn process_line(input: &str) -> u32 {
+    let hand = input.split_whitespace().nth(0).unwrap();
+    let bid = input.split_whitespace().nth(1).unwrap();
+
+
+
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+        assert_eq!("6440", process(input)?);
+        Ok(())
+    }
+}
