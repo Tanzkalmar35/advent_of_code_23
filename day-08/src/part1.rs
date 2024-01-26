f30250016e86cc74a2accd289dd21aa86ee657b6
Tanzkalmar35 <fabianholler2005@gmail.com>
2024-01-26 22:51:35 +0100

    Started working on day-08

--- day-08/src/part1.rs
+++ day-08/src/part1.rs
@@ -3,17 +3,32 @@ use crate::custom_error::AocError;
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
-    let vec = split_instr_and_seq(&input);
-    let res = vec.iter().map(|line| process_line);
+    let (instr, seq) = split_instr_and_seq(&input);
+    let res = seq.iter().map(|line| extract_dir);
+    for instr in instr.split("") {
+        match instr {
+            String::from("L") => {
+
+            }
+            String::from("R") => {
+
+            }
+            _ => {}
+        }
+    }
     todo!()
 }
 
-pub fn process_line(line: &str) {
+pub fn extract_dir(line: &str) -> Vec<&str> {
+    let right_path = line.split_whitespace().nth(3).unwrap();
+    let left_path = line.split_whitespace().nth(2).unwrap();
+    vec![left_path, right_path]
 
 }
 
-fn split_instr_and_seq(input: &str) -> Vec<Vec<&str>> {
-    todo!()
+fn split_instr_and_seq(input: &str) -> (Vec<String> ,Vec<String>) {
+    let lines = input.lines();
+    (lines[0], lines[2..])
 }
 
 #[cfg(test)]
